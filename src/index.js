a7f99f530fd5d084812be83c653341712b54d3cf
Wave <372969383@qq.com>
2017-07-14 15:56:28 +0800

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './views/app/index'
-//import router from './router'
+import App from './views/login/index'
 import Component from 'vue-class-component'
 import ElementUI from 'element-ui'
+import './style/index'
 
 Vue.config.productionTip = false
-/**
- * 此处为入口文件
- */
-
 /* eslint-disable no-new */
 
 @Component({
