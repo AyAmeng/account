e660b0b3ebfbc8cf6837b1c6404fc326ac0b2bd1
Wave <372969383@qq.com>
2017-07-17 10:40:20 +0800

    next

--- src/next-deom/.next/bundles/pages/_error.js
+++ src/next-deom/.next/bundles/pages/_error.js
@@ -3,15 +3,15 @@
             var comp = module.exports =
 webpackJsonp([1],{
 
-/***/ 539:
+/***/ 530:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(230);
+module.exports = __webpack_require__(531);
 
 
 /***/ })
 
-},[539]);
+},[530]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlcy9wYWdlcy9fZXJyb3IuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiI7QTs7Ozs7Ozs7OztBIiwic291cmNlUm9vdCI6IiJ9
             return { page: comp.default }
           })
---
+++ src/next-deom/pages/index.js
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>My Blog</div>
+}
