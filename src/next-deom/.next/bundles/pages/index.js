81dce0312227c0f33e090c90b39b3f06dc8394de
Wave <372969383@qq.com>
2017-07-18 11:17:40 +0800

    react-demo

--- src/next-deom/.next/bundles/pages/index.js
+++ src/next-deom/.next/bundles/pages/index.js
@@ -14,7 +14,7 @@ module.exports = __webpack_require__(555);
 /***/ 555:
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: SyntaxError: Unexpected token (2:9)\n\n\u001b[0m \u001b[90m 1 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m () \u001b[33m=>\u001b[39m {\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 2 | \u001b[39m  \u001b[36mreturn\u001b[39m \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[33mMy\u001b[39m \u001b[33mBlog\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m   | \u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 3 | \u001b[39m}\n \u001b[90m 4 | \u001b[39m\u001b[0m\n");
+throw new Error("Module build failed: SyntaxError: Unexpected token (5:9)\n\n\u001b[0m \u001b[90m 3 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m \u001b[36mclass\u001b[39m \u001b[36mextends\u001b[39m \u001b[33mReact\u001b[39m\u001b[33m.\u001b[39m\u001b[33mComponent\u001b[39m {\n \u001b[90m 4 | \u001b[39m  render () {\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 5 | \u001b[39m\t\t\u001b[36mreturn\u001b[39m \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[33mMy\u001b[39m \u001b[33mBlog\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m   | \u001b[39m\t\t       \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 6 | \u001b[39m\t\t\tconsole\u001b[33m.\u001b[39minfo(\u001b[32m'222'\u001b[39m)\n \u001b[90m 7 | \u001b[39m\t\t}\n \u001b[90m 8 | \u001b[39m}\u001b[0m\n");
 
 /***/ })
 
--- src/next-deom/pages/index.js
+++ src/next-deom/pages/index.js
@@ -1,3 +1,8 @@
-export default () => {
-  return <div>My Blog</div>
+
+import React from 'react'
+export default class extends React.Component {
+  render () {
+		return <div>My Blog</div>
+			console.info('222')
+		}
 }
